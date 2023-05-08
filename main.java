443d678af4c18434e64f436d7cc0ecd40d15cf5c
Ben Caunt <bdcaunt@gmail.com>
2023-05-08 13:24:58 -0400

    first commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+}
